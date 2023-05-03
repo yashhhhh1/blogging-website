61828cfd739f61ff1cb806fad07f88ca6ab5b1f0
yashhhhh1 <yash8511132161@gmail.com>
2023-05-03 16:50:52 +0530

    signup and signin page with datababse added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const UserRoute = require('./routes/user');
+const { log } = require('console');
+
+
+const app= express();
+const PORT=8000;
+
+app.use(express.urlencoded({extended:false}))
+
+mongoose.connect('mongodb://127.0.0.1:27017/blogify').then(e =>{
+    console.log("mongodb is conneced");
+})
+
+app.set('view engine' ,'ejs');
+app.set('views' , path.resolve("./views"));
+
+app.get('/' , (req,res) =>{ 
+    res.render('home');
+})
+
+app.use('/user' , UserRoute)
+
+app.listen(PORT , ()=>{
+    console.log(`Server Started at port ${PORT}`);
+})
